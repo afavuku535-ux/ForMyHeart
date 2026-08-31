b129a8314e380d2e27242a52fdea2a7c58a54e66
afavuku535-ux <afavuku535@gmail.com>
2026-08-31 07:09:54 +0330

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,6 @@
 // ============================================================
-// اطلاعات پروژه‌ی Firebase خودت رو اینجا جایگزین کن.
-// از Firebase Console -> Project settings -> your apps -> SDK setup
-// می‌تونی این مقادیر رو کپی کنی.
-// فقط Realtime Database لازمه، به Storage نیازی نیست.
+// فقط همین یه خط لازمه: آدرس Realtime Database خودت
 // ============================================================
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDVIMKiPVgooVtemW20XfVGfUCzRJo_Krs",
-  authDomain: "formyheart-d6918.firebaseapp.com",
-  databaseURL: "https://formyheart-d6918-default-rtdb.firebaseio.com",
-  projectId: "formyheart-d6918",
-  storageBucket: "formyheart-d6918.firebasestorage.app",
-  messagingSenderId: "501207449096",
-  appId: "1:501207449096:web:bdcbd69bf758809a5c9bbd"
-};
+const FIREBASE_DB_URL = "https://formyheart-d6918-default-rtdb.firebaseio.com";
+
